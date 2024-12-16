a736da84429659e6c18a3680b21b0c48865e1c96
carolinaminguzzi <carolina.minguzzi@gmail.com>
2024-12-16 11:57:53 +0100

    modifiche TimerView
    aggiunta picker

--- clock/clock/ClockAppView.swift
+++ clock/clock/ClockAppView.swift
@@ -32,6 +32,7 @@ struct ClockAppView: View {
                     Label("Timer", systemImage: "timer")
                 }
         }
+        
     }
 }
 
@@ -118,29 +119,42 @@ struct AlarmView: View {
 }
 
 
-
 struct StopwatchView: View {
     @State private var isRunning = false
     @State private var timeElapsed: TimeInterval = 0
     @State private var timer: Timer? = nil
     @State private var hapticTimer: Timer? = nil
+    @GestureState private var isPressing = false // Stato per il gesto di pressione
 
     var body: some View {
         VStack {
             HStack {
+                // Bottone Start/Stop con LongPressGesture
                 Button(action: {
-                    if isRunning {
-                        stopTimer()
-                    } else {
-                        startTimer()
-                    }
+                    // Non fare nulla se il tocco non è stato mantenuto abbastanza a lungo
                 }) {
                     Text(isRunning ? "Stop" : "Start")
                         .frame(width: 70, height: 70)
-                        .background(isRunning ? Color.red : Color.green)
-                        .foregroundColor(.white)
+                        .background(isPressing ? Color.blue : (isRunning ? Color.green : Color.red))
+                        .foregroundColor(isPressing ? Color.white : (isRunning ? Color.red : Color.green))
                         .clipShape(Circle())
+                        .bold()
+                        .scaleEffect(isPressing ? 1.2 : 1.0) // Aggiungi un effetto di ingrandimento quando si tiene premuto
+                        
                 }
+                .simultaneousGesture(
+                    LongPressGesture(minimumDuration: 1.0) // 1 secondo per avviare il timer
+                        .updating($isPressing) { value, state, _ in
+                            state = value
+                        }
+                        .onEnded { _ in
+                            if isRunning {
+                                stopTimer()
+                            } else {
+                                startTimer()
+                            }
+                        }
+                )
 
                 Spacer()
 
@@ -211,47 +225,122 @@ struct StopwatchView: View {
 
 
 struct TimerView: View {
-    @State private var timeRemaining: TimeInterval = 60
+    @State private var timeRemaining: TimeInterval = 0
     @State private var isRunning = false
     @State private var timer: Timer? = nil
 
+    @State private var selectedHours: Int = 0
+    @State private var selectedMinutes: Int = 0
+    @State private var selectedSeconds: Int = 0
+
     var body: some View {
         VStack {
-            Text(formatTime(timeRemaining))
+            Text("Timers")
                 .font(.largeTitle)
-                .padding()
+                .bold()
+                .padding(.top, 40)
 
-            HStack {
+            Spacer()
+
+            // Picker for Hours, Minutes, Seconds con sfondo unico
+            ZStack {
+                // Background per l'intero contenitore dei picker
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.gray.opacity(0.2)) // Colore di sfondo grigio
+                    .frame(height: 150) // Altezza del contenitore
+
+                HStack(spacing: 0) {
+                    VStack {
+                        Picker("Hours", selection: $selectedHours) {
+                            ForEach(0..<25, id: \.self) { hour in
+                                Text("\(hour)").tag(hour)
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(width: 100)
+                        .clipped()
+                        Text("hours")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                    }
+
+                    VStack {
+                        Picker("Minutes", selection: $selectedMinutes) {
+                            ForEach(0..<60, id: \.self) { minute in
+                                Text("\(minute)").tag(minute)
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(width: 100)
+                        .clipped()
+                        Text("min")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                    }
+
+                    VStack {
+                        Picker("Seconds", selection: $selectedSeconds) {
+                            ForEach(0..<60, id: \.self) { second in
+                                Text("\(second)").tag(second)
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(width: 100)
+                        .clipped()
+                        Text("sec")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                    }
+                }
+                .padding(.horizontal, 20)
+            }
+            .padding()
+    
+
+            // Buttons Start and Reset
+            HStack(spacing: 60) {
                 Button(action: {
-                    provideHapticFeedback()
                     if isRunning {
                         stopTimer()
                     } else {
                         startTimer()
                     }
                 }) {
-                    Text(isRunning ? "Pause" : "Start")
-                        .frame(width: 70, height: 70)
+                    Text(isRunning ? "Stop" : "Start")
+                        .frame(width: 100, height: 100)
                         .background(isRunning ? Color.red : Color.green)
                         .foregroundColor(.white)
                         .clipShape(Circle())
+                        .font(.headline)
                 }
-
-                Button("Reset") {
-                    provideHapticFeedback()
+                
+                Button(action: {
+                    generateHapticFeedback()
                     resetTimer()
+                }) {
+                    Text("Reset")
+                        .frame(width: 100, height: 100)
+                        .background(Color.gray)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .font(.headline)
                 }
-                .frame(width: 70, height: 70)
-                .background(Color.gray)
-                .foregroundColor(.white)
-                .clipShape(Circle())
                 .disabled(isRunning)
             }
-            .padding()
+            .padding(.bottom, 50)
+
+            Spacer()
         }
     }
 
     func startTimer() {
+        timeRemaining = TimeInterval((selectedHours * 3600) + (selectedMinutes * 60) + selectedSeconds)
         isRunning = true
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             if timeRemaining > 0 {
@@ -269,21 +358,22 @@ struct TimerView: View {
     }
 
     func resetTimer() {
-        timeRemaining = 60
+        stopTimer()
+        selectedHours = 0
+        selectedMinutes = 0
+        selectedSeconds = 0
+        timeRemaining = 0
     }
 
-    func formatTime(_ time: TimeInterval) -> String {
-        let minutes = Int(time) / 60
-        let seconds = Int(time) % 60
-        return String(format: "%02d:%02d", minutes, seconds)
-    }
-
-    func provideHapticFeedback() {
-        let generator = UIImpactFeedbackGenerator(style: .heavy)
+    func generateHapticFeedback() {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
         generator.impactOccurred()
     }
-    
 }
+
+
+
+
 
 struct ClockAppView_Previews: PreviewProvider {
     static var previews: some View {

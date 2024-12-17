5805c9652763201432e26f059368730eb503b4b4
carolinaminguzzi <carolina.minguzzi@gmail.com>
2024-12-17 10:48:39 +0100

    Colors
    modifiche sfondi e feedback aptici/sonori

--- clock/clock/ClockAppView.swift
+++ clock/clock/ClockAppView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UIKit
+import AVFoundation
 
 
 struct ClockAppView: View {
@@ -23,69 +24,90 @@ struct ClockAppView: View {
                     Label("Timer", systemImage: "timer")
                 }
         }
-        
+    
     }
+    
 }
 
 
 struct StopwatchView: View {
     @State private var isRunning = false
-    @State private var timeElapsed: TimeInterval = 0
+    @State private var timeElapsed: TimeInterval = 0 // Tempo conteggiato in background
+    @State private var displayedTime: TimeInterval = 0 // Tempo mostrato all'utente
     @State private var timer: Timer? = nil
     @State private var hapticTimer: Timer? = nil
-    @GestureState private var isPressing = false // Stato per il gesto di pressione
+    @State private var isButtonPressed = false // Stato per controllare l'ingrandimento del bottone
 
     var body: some View {
-        VStack {
-            HStack {
-                // Bottone Start/Stop con LongPressGesture
-                Button(action: {
-                    // Non fare nulla se il tocco non è stato mantenuto abbastanza a lungo
-                }) {
-                    Text(isRunning ? "Stop" : "Start")
-                        .frame(width: 70, height: 70)
-                        .background(isPressing ? Color.blue : (isRunning ? Color.green : Color.red))
-                        .foregroundColor(isPressing ? Color.white : (isRunning ? Color.red : Color.green))
-                        .clipShape(Circle())
-                        .bold()
-                        .scaleEffect(isPressing ? 1.2 : 1.0) // Aggiungi un effetto di ingrandimento quando si tiene premuto
-                        
-                }
-                .simultaneousGesture(
-                    LongPressGesture(minimumDuration: 1.0) // 1 secondo per avviare il timer
-                        .updating($isPressing) { value, state, _ in
-                            state = value
-                        }
-                        .onEnded { _ in
-                            if isRunning {
-                                stopTimer()
-                            } else {
-                                startTimer()
-                            }
-                        }
-                )
+        ZStack {
+            Color.purple
+                .edgesIgnoringSafeArea(.all) // Sfondo viola
 
+            // Timer centrato nella vista
+            VStack {
                 Spacer()
 
-                Button(action: {
-                    generateHapticFeedback()
-                    resetTimer()
-                }) {
-                    Text("Reset")
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                }
-                .disabled(isRunning)
+                Text(formatTime(displayedTime)) // Mostra solo il tempo fermato
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundColor(.blue)
+
+                Spacer()
             }
-            .padding([.leading, .trailing], 20)
 
-            Spacer()
+            // Bottone Reset in alto a sinistra
+            VStack {
+                HStack {
+                    Button(action: {
+                        generateHapticFeedback()
+                        resetTimer()
+                    }) {
+                        Text("Reset")
+                            .frame(width: 70, height: 70)
+                            .background(Color.blue)
+                            .foregroundColor(.blue)
+                            .clipShape(Circle())
+                            .font(.headline)
+                    }
+                    .padding(.top, 20)
+                    .padding(.leading, 20)
 
-            Text(formatTime(timeElapsed))
-                .font(.largeTitle)
-                .padding()
+                    Spacer()
+                }
+
+                Spacer()
+            }
+
+            // Bottone Start/Stop in basso a sinistra
+            VStack {
+                Spacer()
+
+                HStack {
+                    Button(action: {
+                        isButtonPressed = true
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0)) {
+                            isButtonPressed = false
+                        }
+                        if isRunning {
+                            stopTimer()
+                        } else {
+                            startTimer()
+                        }
+                    }) {
+                        Text(isRunning ? "Stop" : "Start")
+                            .frame(width: 70, height: 70)
+                            .background(isRunning ? Color.blue : Color.blue)
+                            .foregroundColor(.blue)
+                            .clipShape(Circle())
+                            .bold()
+                            .scaleEffect(isButtonPressed ? 1.2 : 1.0) // Ingrandisce quando premuto
+                    }
+                    .padding(.bottom, 20)
+                    .padding(.leading, 20)
+
+                    Spacer()
+                }
+            }
         }
     }
 
@@ -102,10 +124,14 @@ struct StopwatchView: View {
         timer?.invalidate()
         timer = nil
         stopHapticFeedbackLoop()
+
+        // Aggiorna il valore mostrato quando si preme "Stop"
+        displayedTime = timeElapsed
     }
 
     func resetTimer() {
         timeElapsed = 0
+        displayedTime = 0
     }
 
     func formatTime(_ time: TimeInterval) -> String {
@@ -133,127 +159,94 @@ struct StopwatchView: View {
 
 
 
+
 struct TimerView: View {
     @State private var timeRemaining: TimeInterval = 0
     @State private var isRunning = false
     @State private var timer: Timer? = nil
-
+    @State private var rotationAngle: Double = 0 // Per il cerchio di caricamento
     @State private var selectedHours: Int = 0
     @State private var selectedMinutes: Int = 0
     @State private var selectedSeconds: Int = 0
 
     var body: some View {
-        VStack {
-            Text("Timers")
-                .font(.largeTitle)
-                .bold()
-                .padding(.top, 40)
+        ZStack {
+            // Sfondo verde per tutta la view
+            Color.green
+                .edgesIgnoringSafeArea(.all)
 
-            Spacer()
+            VStack {
+                Spacer()
 
-            // Picker for Hours, Minutes, Seconds con sfondo unico
-            ZStack {
-                // Background per l'intero contenitore dei picker
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(Color.gray.opacity(0.2)) // Colore di sfondo grigio
-                    .frame(height: 150) // Altezza del contenitore
-
-                HStack(spacing: 0) {
-                    VStack {
-                        Picker("Hours", selection: $selectedHours) {
-                            ForEach(0..<25, id: \.self) { hour in
-                                Text("\(hour)").tag(hour)
-                            }
-                        }
-                        .pickerStyle(.wheel)
-                        .frame(width: 100)
-                        .clipped()
-                        Text("hours")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
+                // Picker con numeri gialli quando selezionati
+                ZStack {
+                    HStack(spacing: 0) {
+                        PickerColumn(title: "hours", range: 0..<25, selection: $selectedHours)
+                        PickerColumn(title: "min", range: 0..<60, selection: $selectedMinutes)
+                        PickerColumn(title: "sec", range: 0..<60, selection: $selectedSeconds)
                     }
+                    .padding(.horizontal, 20)
+                }
+                .padding()
 
-                    VStack {
-                        Picker("Minutes", selection: $selectedMinutes) {
-                            ForEach(0..<60, id: \.self) { minute in
-                                Text("\(minute)").tag(minute)
-                            }
-                        }
-                        .pickerStyle(.wheel)
-                        .frame(width: 100)
-                        .clipped()
-                        Text("min")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                    }
+                Spacer()
 
-                    VStack {
-                        Picker("Seconds", selection: $selectedSeconds) {
-                            ForEach(0..<60, id: \.self) { second in
-                                Text("\(second)").tag(second)
-                            }
-                        }
-                        .pickerStyle(.wheel)
-                        .frame(width: 100)
-                        .clipped()
-                        Text("sec")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
+                // Cerchio di caricamento visibile solo quando il timer è in esecuzione
+                if isRunning {
+                    ZStack {
+                        Circle()
+                            .trim(from: 0, to: 1)
+                            .stroke(Color.yellow, lineWidth: 5)
+                            .frame(width: 150, height: 150)
+                            .rotationEffect(.degrees(rotationAngle))
+                            .animation(.linear(duration: 1), value: rotationAngle) // Animazione fluida
                     }
                 }
-                .padding(.horizontal, 20)
+
+                // Pulsanti
+                HStack(spacing: 60) {
+                    Button(action: {
+                        if isRunning {
+                            stopTimer()
+                        } else {
+                            startTimer()
+                        }
+                    }) {
+                        Text(isRunning ? "Stop" : "Start")
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.yellow)
+                            .clipShape(Circle())
+                            .font(.headline)
+                    }
+
+                    Button(action: {
+                        resetTimer()
+                    }) {
+                        Text("Reset")
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.yellow)
+                            .clipShape(Circle())
+                            .font(.headline)
+                    }
+                    .disabled(isRunning)
+                }
+                .padding(.bottom, 50)
+
+                Spacer()
             }
-            .padding()
-    
-
-            // Buttons Start and Reset
-            HStack(spacing: 60) {
-                Button(action: {
-                    if isRunning {
-                        stopTimer()
-                    } else {
-                        startTimer()
-                    }
-                }) {
-                    Text(isRunning ? "Stop" : "Start")
-                        .frame(width: 100, height: 100)
-                        .background(isRunning ? Color.red : Color.green)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                        .font(.headline)
-                }
-                
-                Button(action: {
-                    generateHapticFeedback()
-                    resetTimer()
-                }) {
-                    Text("Reset")
-                        .frame(width: 100, height: 100)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                        .font(.headline)
-                }
-                .disabled(isRunning)
-            }
-            .padding(.bottom, 50)
-
-            Spacer()
         }
     }
 
     func startTimer() {
         timeRemaining = TimeInterval((selectedHours * 3600) + (selectedMinutes * 60) + selectedSeconds)
         isRunning = true
+        rotationAngle = 0
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             if timeRemaining > 0 {
                 timeRemaining -= 1
+                rotationAngle += 360 // Ruota il cerchio di 360° ogni secondo
+                playSound()
+                generateHapticFeedback()
             } else {
                 stopTimer()
             }
@@ -272,15 +265,47 @@ struct TimerView: View {
         selectedMinutes = 0
         selectedSeconds = 0
         timeRemaining = 0
+        rotationAngle = 0
     }
 
     func generateHapticFeedback() {
-        let generator = UIImpactFeedbackGenerator(style: .medium)
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
         generator.impactOccurred()
+    }
+
+    func playSound() {
+        let systemSoundID: SystemSoundID = 1053 // Suono di beep predefinito
+        AudioServicesPlaySystemSound(systemSoundID)
     }
 }
 
+struct PickerColumn: View {
+    let title: String
+    let range: Range<Int>
+    @Binding var selection: Int
 
+    var body: some View {
+        VStack {
+            Picker(title, selection: $selection) {
+                ForEach(range, id: \.self) { value in
+                    Text("\(value)")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(value == selection ? Color.yellow : .primary) // Giallo per il valore selezionato
+                        .scaleEffect(value == selection ? 1.0 : 1.2) // Rende il testo non selezionato più grande
+                        .animation(.easeInOut(duration: 0.3), value: selection) // Animazione fluida
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .pickerStyle(.wheel)
+            .frame(width: 100)
+            .clipped()
+
+            Text(title)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+    }
+}
 
 
 
